b01e946c1cae854d7c1d7f45f71b471b1655f072
Chow Jia Ying <chowjiaying211@gmail.com>
2025-10-24 15:47:15 +0800

    Initial Commit

---
+++ hello sdl2/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  hello sdl2
+//
+//  Created by Chow Jia Ying on 24/10/25.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
