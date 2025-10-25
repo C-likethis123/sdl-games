18cee43a19b2546cc31df1b40087c2e826f14b6f
Chow Jia Ying <chowjiaying211@gmail.com>
2025-10-25 14:00:20 +0800

    finally set up everything

--- hello sdl2/hello.cpp
+++ hello sdl2/hello.cpp
@@ -1,5 +1,8 @@
 #include <SDL3/SDL.h>
 #include <SDL3/SDL_main.h>
+#include <SDL3_image/SDL_image.h>
+#include <SDL3_ttf/SDL_ttf.h>
+#include <SDL3_mixer/SDL_mixer.h>
 #include <string>
 
 const int SCREEN_WIDTH = 640;
